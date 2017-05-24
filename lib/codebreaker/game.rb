ea7f31383871af200a983f37408e9927afefa74b
Yevhen Radionov <eugene.radionov@gmail.com>
2017-05-24 17:02:03 +0300

    Added again?, save_score and go methods.

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -39,17 +39,64 @@ module Codebreaker
     end
 
     def play
+      start
       menu
       choice = gets.chomp[/\d/]
-      return nil if choice.to_i.zero?
+      exit(0) if choice.to_i.zero?
 
       @attempts = 6
+      @attempts_used = 0
       @attempts.times do
         enter_number
+        @attempts_used += 1
         return 1 if check == 1
+        puts check
       end
       return -1
     end
 
+    def again?
+      puts 'Do you want to play again(y or n) or save score(s)?'
+      choice = gets.chomp.downcase[/^[yns]/]
+
+      case choice
+        when 'y'
+          go
+        when 'n'
+          puts 'Bye!'
+          sleep(0.5)
+          exit 0
+        when 's'
+          puts 'What is your name?'
+          name = gets.chomp
+          save_score(name)
+        else
+          save_score
+          puts 'Your score was saved.'
+          puts 'Bye!'
+          sleep(0.3)
+          exit 0
+      end
+    end
+
+    def save_score(name = 'Unknown', file_name = 'codebreaker_score.txt')
+      score = (@attempts_used.to_f / @attempts * 100).round(2)
+      File.new(file_name, 'w') unless File.exist?(file_name)
+      File.open(file_name, 'w') do |file|
+        file.write("CODEBREAKER SCORE\n")
+        file.write("Name: #{name}\n")
+        file.write("Score: #{score}%\n")
+      end
+    end
+
+    def go
+      if play > 0
+        puts 'You won!'
+      else
+        puts 'You lost!'
+      end
+
+      again?
+    end
   end
 end

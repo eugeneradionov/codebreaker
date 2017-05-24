e059fb007055142b413bb991e8f92e945b19be5d
Yevhen Radionov <eugene.radionov@gmail.com>
2017-05-24 12:31:07 +0300

    Added tests for #play method.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,13 +21,14 @@ module Codebreaker
         expect(game.instance_variable_get(:@secret_code)).to match(/[1-6]{4}/)
       end
     end
-    
-    context '#get_number' do
+
+    context '#enter_number' do
       let(:game) { Game.new }
 
       before do
         allow(game).to receive(:gets).and_return('1234')
-        game.get_number
+        allow(game).to receive(:puts) { 'Please enter your number' }
+        game.enter_number
       end
 
       it 'saves input only with 4 numbers from 1 to 6' do
@@ -40,10 +41,11 @@ module Codebreaker
 
       before do
         allow(game).to receive(:gets).and_return('1234')
-        game.get_number
+        allow(game).to receive(:puts) { 'Please enter your number' }
+        game.enter_number
       end
 
-      it 'when did not guessed exactly' do # returns a string of pluses/minuses or empty
+      it 'when did not guessed exactly' do
         game.instance_variable_set(:@secret_code, '4321')
         expect(game.check).to match(/^[+-]*$/)
       end
@@ -53,5 +55,52 @@ module Codebreaker
         expect(game.check).to eq(1)
       end
     end
+
+    context '#play' do
+      let(:game) { Game.new }
+
+      before do
+        allow(game).to receive(:puts).and_return('Menu')
+      end
+
+      it 'plays when choice 1' do
+        allow(game).to receive(:gets).and_return('1')
+        allow(game).to receive(:gets).and_return('1234')
+        game.play
+      end
+
+      it 'should exit when choice 0' do
+        allow(game).to receive(:gets).and_return('0')
+        expect(game.play).to eq(nil)
+      end
+
+      it 'has more than 0 attempts' do
+        allow(game).to receive(:gets).and_return('1')
+        allow(game).to receive(:gets).and_return('1234')
+        game.play
+        expect(game.instance_variable_get(:@attempts)).to be > 0
+      end
+
+      it 'looses when attempts is over' do
+        allow(game).to receive(:gets).and_return('1')
+        allow(game).to receive(:gets).and_return('1234')
+        game.instance_variable_set(:@secret_code, '4321')
+        expect(game.play).to eq(-1)
+      end
+
+      it 'wins when secret code exactly guessed' do
+        allow(game).to receive(:gets).and_return('1')
+        allow(game).to receive(:gets).and_return('1234')
+        game.instance_variable_set(:@secret_code, '1234')
+        expect(game.play).to eq(1)
+      end
+
+      it 'ends either win or loose' do
+        allow(game).to receive(:gets).and_return('1')
+        allow(game).to receive(:gets).and_return('1234')
+        expect(game.play.class).to eq(Integer)
+      end
+    end
+
   end
 end
